ec9e124161c7abf62f629077b3bbba8315058837
yousef-attata <yousef@approdite.com>
2019-08-26 20:17:35 +0300

    Sign up

---
+++ src/screens/SignUp/ChoiceScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import TextInput from '../../components/TextInput';
+
+const SignUp = () => {
+  return (
+    <TextInput bg='color' />
+  );
+};
+
+export default SignUp;
