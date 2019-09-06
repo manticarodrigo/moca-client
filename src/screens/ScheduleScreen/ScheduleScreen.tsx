6e11b7593b0248e393939f10928a89cc54960fa7
Rodrigo Mantica <manticarodrigo@Rodrigos-MacBook-Pro.local>
2019-09-05 21:30:16 -0400

    Calendar boilerplate.

--- src/screens/ScheduleScreen/ScheduleScreen.tsx
+++ src/screens/ScheduleScreen/ScheduleScreen.tsx
@@ -1,20 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Calendar } from 'react-native-calendars';
 
-import useNavigation from '@src/hooks/useNavigation';
-
+import { Colors, Typography } from '@src/styles';
 import View from '@src/components/View';
-import Button from '@src/components/Button';
 
 const ScheduleScreen = () => {
-  const navigation = useNavigation();
+  const [markedDates, setMarkedDates] = useState({});
 
-  const handleButtonPress = () => navigation.navigate('ChatListScreen');
+  const onDayPress = (day) => {
+    setMarkedDates((prev) => {
+      const { length } = Object.keys(prev);
+
+      if (!length) {
+        return {
+          ...prev,
+          [day.dateString]: {
+            startingDay: true,
+            selected: true,
+            color: Colors.primary,
+          },
+        };
+      }
+
+      if (length === 1) {
+        return {
+          ...prev,
+          [day.dateString]: {
+            endingDay: true,
+            selected: true,
+            color: Colors.primary,
+          },
+        };
+      }
+
+      if (length > 1) {
+        return {
+          [day.dateString]: {
+            startingDay: true,
+            selected: true,
+            color: Colors.primary,
+          },
+        };
+      }
+
+      return prev;
+    });
+  };
 
   return (
     <View flex={1} justifyCenter alignCenter bgColor="lightGrey">
-      <Button onPress={handleButtonPress}>
-        Go to Chat
-      </Button>
+      <Calendar
+        markedDates={markedDates}
+        markingType="period"
+        onDayPress={onDayPress}
+        style={{ width: '100%', height: '100%' }}
+        theme={{
+          calendarBackground: Colors.semiGreyLighter,
+          textSectionTitleColor: Colors.greyishBrown,
+          arrowColor: Colors.secondary,
+          dayTextColor: Colors.greyishBrown,
+          textDayFontFamily: 'family-700',
+          todayTextColor: Colors.secondary,
+          'stylesheet.calendar.header': {
+            header: {
+              backgroundColor: Colors.primary,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              padding: 10,
+              marginLeft: -5,
+              marginRight: -5,
+              alignItems: 'center',
+            },
+            monthText: {
+              ...Typography.getStyles({ size: 2, weight: '700', color: 'white' }),
+              margin: 10,
+            },
+            arrow: {
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: 32,
+              height: 32,
+              backgroundColor: Colors.primaryTranslucent,
+              borderRadius: 16,
+            },
+            dayHeader: {
+              ...Typography.getStyles({
+                size: 0,
+                weight: '300',
+                color: 'greyishBrown',
+                transform: 'uppercase',
+                align: 'center',
+              }),
+              marginTop: 10,
+              marginBottom: 10,
+              width: 35,
+            },
+          },
+        }}
+      />
     </View>
   );
 };
