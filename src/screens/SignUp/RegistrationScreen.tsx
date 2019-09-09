288e8806d5499b84cdcd1837a8da067b927c6cbf
yousef-attata <yousef@approdite.com>
2019-09-09 13:21:04 +0300

    Modified signUp screen

--- src/screens/SignUp/RegistrationScreen.tsx
+++ src/screens/SignUp/RegistrationScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, KeyboardAvoidingView } from 'react-native';
+import { Header } from 'react-navigation';
 
 import useNavigation from '@src/hooks/useNavigation';
 import useStore from '@src/hooks/useStore';
@@ -33,6 +34,7 @@ const RegistrationScreen = () => {
   const [isButtonPressed, setIsButtonPressed] = useState(false);
   const isPatient = userInformation.type === 'Patient';
 
+
   const handleButtonPress = () => {
     // validation
     dispatch(updateUserInfomation({ ...formFields }));
@@ -65,12 +67,12 @@ const RegistrationScreen = () => {
   };
 
   const mediCare = (
-    <View row spacing={{ m: 3 }}>
-      <View spacing={{ mr: 3 }}>
+    <View row spacing={{ px: 3, py: 4 }} justifyBetween width="100%" variant="borderTop">
+      <View>
         <Text variant="title" typography={{ size: 2 }}>Are you currently</Text>
         <Text variant="title" typography={{ size: 2 }}>covered by Medicare?</Text>
       </View>
-      <View row alignCenter>
+      <View row>
         <Button variant="tertiary" onPress={handleMedicareAgreement}>
           Yes
         </Button>
@@ -88,80 +90,99 @@ const RegistrationScreen = () => {
       placeholder="Medical Id"
       value={formFields.medicalId}
       returnKeyType="next"
+      keyboardType="number-pad"
       onChangeText={(text) => handleFormFields('medicalId', text)}
     />
   );
 
 
   return (
-    <View safeArea flex={1} justifyBetween width="100%" spacing={{ mt: 3 }}>
-      <StatusBar barStyle="dark-content" />
-      <View alignCenter>
-        <Image file={logoIcon} width={51} height={59} />
-        <Text variant="title" spacing={{ mt: 3 }}>Moca is available in your area</Text>
-        <Text variant="regular" spacing={{ mt: 1 }}>
-          We need some infromation to
-        </Text>
-        <Text variant="regular">
-          get you started.
-        </Text>
-        <View variant="borderTop" width="100%" spacing={{ mt: 3 }}>
-          {isPatient && mediCare}
-        </View>
-      </View>
-      <View alignCenter spacing={{ mb: 3 }}>
-        <FormField
-          placeholder="Name"
-          value={formFields.name}
-          returnKeyType="next"
-          onChangeText={(text) => handleFormFields('name', text)}
-        />
-        <FormField
-          placeholder="Surname"
-          value={formFields.surname}
-          returnKeyType="next"
-          onChangeText={(text) => handleFormFields('surname', text)}
-        />
-        {!isPatient && medicalId}
-        <FormField
-          placeholder="Email address"
-          value={formFields.email}
-          returnKeyType="next"
-          keyboardType="email-address"
-          onChangeText={(text) => handleFormFields('email', text)}
-          icon={EmailIcon}
-        />
-        <FormField
-          placeholder="password"
-          value={formFields.password}
-          secureTextEntry
-          returnKeyType="done"
-          onChangeText={(text) => handleFormFields('password', text)}
-          icon={PasswordIcon}
-        />
-      </View>
-      <View spacing={{ mx: 3 }}>
-        <Button onPress={handleButtonPress}>
-          Continue
-        </Button>
-        <View alignCenter row spacing={{ mt: 1 }}>
-          <Text variant="regular" typography={{ size: 2 }}>
-            {'By continuing, I accept the Moca '}
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior="padding"
+    >
+      <View safeArea flex={1} spacing={{ mt: 4 }} justifyEnd>
+        <StatusBar barStyle="dark-content" />
+        <View alignCenter>
+          <Image file={logoIcon} width={49} height={60} />
+          <Text variant="title" spacing={{ mt: 3 }}>Moca is available in your area</Text>
+          <Text variant="regular" spacing={{ mt: 2 }}>
+            We need some infromation to
           </Text>
-          <Text variant="link" onPress={handleTermsPress} typography={{ size: 1 }}>
-            terms of service
+          <Text variant="regular" spacing={{ mt: 1, mb: 2 }}>
+            get you started.
           </Text>
         </View>
-        <View alignCenter row spacing={{ mt: 1, mb: 2 }}>
-          <Text variant="regular" typography={{ size: 1 }}>
-            {'and have read the '}
-          </Text>
-          <Text variant="link" onPress={handlePrivaryPress} typography={{ size: 1 }}>
-            privacy policy
-          </Text>
+        {isPatient && mediCare}
+        <View spacing={{ mb: 3 }}>
+          <FormField
+            placeholder="Name"
+            value={formFields.name}
+            returnKeyType="next"
+            onChangeText={(text) => handleFormFields('name', text)}
+          />
+          <FormField
+            placeholder="Surname"
+            value={formFields.surname}
+            returnKeyType="next"
+            onChangeText={(text) => handleFormFields('surname', text)}
+          />
+          {!isPatient && medicalId}
+          <FormField
+            placeholder="Email address"
+            value={formFields.email}
+            returnKeyType="next"
+            keyboardType="email-address"
+            onChangeText={(text) => handleFormFields('email', text)}
+            icon={EmailIcon}
+          />
+          <FormField
+            placeholder="password"
+            value={formFields.password}
+            secureTextEntry
+            returnKeyType="done"
+            onChangeText={(text) => handleFormFields('password', text)}
+            icon={PasswordIcon}
+          />
         </View>
+        <View spacing={{ mx: 3 }}>
+          <Button onPress={handleButtonPress}>
+            Continue
+          </Button>
+          <View alignCenter row spacing={{ mt: 2 }}>
+            <Text
+              variant="regular"
+              spacing={{ mt: 1 }}
+              typography={{ size: 1 }}
+            >
+              {'By continuing, I accept the Moca'}
+            </Text>
+            <Text
+              variant="link"
+              onPress={handleTermsPress}
+              typography={{ size: 1, color: 'secondary' }}
+              spacing={{ ml: 1, mt: 1 }}
+            >
+              terms of service
+            </Text>
+          </View>
+          <View alignCenter row spacing={{ mt: 1, mb: 2 }}>
+            <Text variant="regular" typography={{ size: 1 }}>
+              and have read the
+            </Text>
+            <Text
+              variant="link"
+              onPress={handlePrivaryPress}
+              typography={{ size: 1, color: 'secondary' }}
+              spacing={{ ml: 1 }}
+            >
+              privacy policy
+            </Text>
+          </View>
+        </View>
+        <View flex={1} />
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
