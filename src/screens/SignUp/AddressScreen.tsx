ed313225aabeaf780cc497e88d4520589ecc1a0a
yousef-attata <yousef@approdite.com>
2019-09-09 13:32:18 +0300

    Added avoidkeyboardView to Address

--- src/screens/SignUp/AddressScreen.tsx
+++ src/screens/SignUp/AddressScreen.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { KeyboardAvoidingView } from 'react-native';
+import { Header } from 'react-navigation';
+
 
 import BackButton from '@src/components/BackButton';
 import View from '@src/components/View';
@@ -32,38 +35,44 @@ const AddressScreen = () => {
   };
 
   return (
-    <View safeArea flex={1} justifyBetween width="100%" spacing={{ mt: 3 }}>
-      <View alignCenter>
-        <View row>
-          <Text variant="title" spacing={{ mt: 3 }}>Thanks for signing up, </Text>
-          <Text variant="title" spacing={{ mt: 3 }}>{name}</Text>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior="padding"
+      keyboardVerticalOffset={Header.HEIGHT + 80}
+    >
+      <View safeArea flex={1} justifyBetween width="100%" spacing={{ mt: 3 }}>
+        <View alignCenter>
+          <View row>
+            <Text variant="title" spacing={{ mt: 3 }}>Thanks for signing up, </Text>
+            <Text variant="title" spacing={{ mt: 3 }}>{name}</Text>
+          </View>
+          <Text variant="regular" spacing={{ mt: 1 }}>
+            What is your preferred address for treatment?
+          </Text>
         </View>
-        <Text variant="regular" spacing={{ mt: 1 }}>
-          What is your preferred address for treatment?
-        </Text>
+        <View alignCenter>
+          <FormField
+            placeholder="Street"
+            value={street}
+            returnKeyType="next"
+            onChangeText={(text) => setStreet(text)}
+            icon={StreetIcon}
+          />
+          <FormField
+            placeholder="Apartment"
+            value={apartment}
+            returnKeyType="done"
+            onChangeText={(text) => setApartment(text)}
+            icon={ApartmentIcon}
+          />
+        </View>
+        <View spacing={{ mx: 3 }}>
+          <Button onPress={handleButtonPress}>
+            Continue
+          </Button>
+        </View>
       </View>
-      <View alignCenter>
-        <FormField
-          placeholder="Street"
-          value={street}
-          returnKeyType="next"
-          onChangeText={(text) => setStreet(text)}
-          icon={StreetIcon}
-        />
-        <FormField
-          placeholder="Apartment"
-          value={apartment}
-          returnKeyType="done"
-          onChangeText={(text) => setApartment(text)}
-          icon={ApartmentIcon}
-        />
-      </View>
-      <View spacing={{ mx: 3 }}>
-        <Button onPress={handleButtonPress}>
-          Continue
-        </Button>
-      </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
