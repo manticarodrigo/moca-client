c20dea2de55e5661d39dc53692124825113a833f
Rodrigo Mantica <manticarodrigo@Rodrigos-MBP.fios-router.home>
2019-09-17 16:02:46 -0400

    Added image viewer.

--- src/screens/ConversationScreen/ConversationMessage.tsx
+++ src/screens/ConversationScreen/ConversationMessage.tsx
@@ -1,5 +1,7 @@
 
-import React, { useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
+import { Modal } from 'react-native';
+import ImageViewer from 'react-native-image-zoom-viewer';
 import { format } from 'date-fns';
 
 import View from '@src/components/View';
@@ -13,35 +15,50 @@ type ConversationMessageProps = {
 
 const ConversationMessage = ({ message, alignRight }: ConversationMessageProps) => {
   const { text, attachmentURI, createdAt } = message;
+  const [imageViewerOpen, setImageViewerOpen] = useState(false);
   const time = useMemo(() => format(createdAt, 'hh:mm'), [createdAt]);
 
+  const toggleImageViewer = () => setImageViewerOpen((prev) => !prev);
+
   return (
-    <View column variant={alignRight ? 'msgBubbleRight' : 'msgBubbleLeft'}>
+    <>
+      <View column variant={alignRight ? 'msgBubbleRight' : 'msgBubbleLeft'}>
+        {attachmentURI && (
+          <View column spacing={{ mb: 2 }} onPress={toggleImageViewer}>
+            <Image width={200} height={200} uri={attachmentURI} />
+          </View>
+        )}
+        <Text
+          typography={{
+            color: alignRight ? 'white' : 'dark',
+            weight: '500',
+            align: 'right',
+          }}
+        >
+          {text}
+        </Text>
+        <Text
+          spacing={{ mt: 2 }}
+          typography={{
+            color: alignRight ? 'secondaryLighter' : 'semiGreyAlt',
+            weight: '500',
+            align: 'right',
+          }}
+        >
+          {time}
+        </Text>
+      </View>
       {attachmentURI && (
-        <View column spacing={{ mb: 2 }}>
-          <Image width={200} height={200} uri={attachmentURI} />
-        </View>
+        <Modal visible={imageViewerOpen} transparent>
+          <ImageViewer
+            imageUrls={[{ url: attachmentURI }]}
+            enableImageZoom
+            enableSwipeDown
+            onSwipeDown={toggleImageViewer}
+          />
+        </Modal>
       )}
-      <Text
-        typography={{
-          color: alignRight ? 'white' : 'dark',
-          weight: '500',
-          align: 'right',
-        }}
-      >
-        {text}
-      </Text>
-      <Text
-        spacing={{ mt: 2 }}
-        typography={{
-          color: alignRight ? 'secondaryLighter' : 'semiGreyAlt',
-          weight: '500',
-          align: 'right',
-        }}
-      >
-        {time}
-      </Text>
-    </View>
+    </>
   );
 };
 
