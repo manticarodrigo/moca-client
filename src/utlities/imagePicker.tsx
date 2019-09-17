8943fbd50b01de1ad002beed17a3123f5ab5de50
Rodrigo Mantica <manticarodrigo@Rodrigos-MBP.fios-router.home>
2019-09-17 17:13:14 -0400

    Refactored imagePicker utility and added alerts.

--- src/utlities/imagePicker.tsx
+++ src/utlities/imagePicker.tsx
@@ -1,31 +1,44 @@
+import { Alert } from 'react-native';
 import ActionSheet from 'react-native-action-sheet';
 import * as Permissions from 'expo-permissions';
 import * as ImagePicker from 'expo-image-picker';
 
 import { Colors } from '@src/styles';
 
+const alertFail = (message: string) => Alert.alert('Image Picker Failed', message);
+
 const getImage = async (callback: (response: ImagePicker.ImagePickerResult) => void) => {
-  const permissions = await Permissions.getAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
+  const { permissions } = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
+  const options = [{ title: 'Cancel', action: () => null }];
 
-  if (permissions.status !== 'granted') {
-    const request = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
-
-    if (request.status !== 'granted') return;
+  if (permissions.camera.status === 'granted') {
+    options.unshift({ title: 'Take Photo', action: ImagePicker.launchCameraAsync });
   }
 
+  if (permissions.cameraRoll.status === 'granted') {
+    options.unshift({ title: 'Photo From Library', action: ImagePicker.launchImageLibraryAsync });
+  }
+
+  if (options.length === 1) {
+    alertFail('You did not grant permissions to use your photos.');
+    return;
+  }
+
+  const lastIndex = options.length - 1;
+
   ActionSheet.showActionSheetWithOptions({
-    options: ['Camera', 'Photos', 'Cancel'],
-    cancelButtonIndex: 2,
+    options: options.map(({ title }) => title),
+    cancelButtonIndex: lastIndex,
     tintColor: Colors.primary,
-  }, async (buttonIndex) => {
+  }, async (index) => {
     let response: ImagePicker.ImagePickerResult = { cancelled: true };
 
-    if (buttonIndex === 0) {
-      response = await ImagePicker.launchCameraAsync();
-    }
-
-    if (buttonIndex === 1) {
-      response = await ImagePicker.launchImageLibraryAsync();
+    try {
+      if (index !== lastIndex) {
+        response = await options[index].action();
+      }
+    } catch (error) {
+      alertFail(error.message);
     }
 
     callback(response);
