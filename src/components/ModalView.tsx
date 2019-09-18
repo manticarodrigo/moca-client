d28a1c7b9e014aa010f333d1576f3a1474220f8d
Rodrigo Mantica <manticarodrigo@Rodrigos-MacBook-Pro.local>
2019-09-18 14:25:26 -0400

    Fixed package lock with yarn.

--- src/components/ModalView.tsx
+++ src/components/ModalView.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-indent-props */
 import React from 'react';
 import Modal, { ModalProps } from 'react-native-modal';
 import OpenIcon from '@src/components/icons/OpenIcon';
@@ -22,15 +21,15 @@ const ModalView = ({
   ...modalProps
 }: BackdropProps) => (
   <Modal
-      {...modalProps}
-      swipeDirection="down"
-      backdropOpacity={0.8}
-      animationInTiming={500}
-      animationOutTiming={500}
-      hideModalContentWhileAnimating
-      animationIn="slideInUp"
-      animationOut="slideOutDown"
-      style={{ margin: 0, marginTop: height }}
+    {...modalProps}
+    swipeDirection="down"
+    backdropOpacity={0.8}
+    animationInTiming={500}
+    animationOutTiming={500}
+    hideModalContentWhileAnimating
+    animationIn="slideInUp"
+    animationOut="slideOutDown"
+    style={{ margin: 0, marginTop: height }}
   >
     <View variant="modal" alignCenter bgColor={bgColor}>
       <View alignCenter spacing={{ mt: 2 }} onPress={handleArrowClick}>
