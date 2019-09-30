ff9deb086e9fc112d755c234965810a01ddb3181
Rodrigo Mantica <manticarodrigo@Rodrigos-MacBook-Pro.local>
2019-09-30 18:47:34 -0400

    Reverted vscode settings. Removed unused components.

--- src/components/HeaderTitle.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import Text from '@src/components/Text';
-
-const HeaderTitle = ({ title }) => <Text variant="titleSmall" spacing={{ ml: 3 }}>{title}</Text>;
-
-export default HeaderTitle;
