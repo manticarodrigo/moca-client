f4c157bef6d9e9113f018073d6c58b82c1e464a5
Rodrigo Mantica <manticarodrigo@Rodrigos-MacBook-Pro.local>
2019-10-09 08:57:39 -0400

    Cleaned up modal component.

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,45 +1,53 @@
-import React from 'react';
-import RNModal, { ModalProps as RNModalProps } from 'react-native-modal';
+import React, { useMemo } from 'react';
+import { StyleSheet } from 'react-native';
+import RNModal, { ModalProps } from 'react-native-modal';
 
 import OpenIcon from '@src/components/icons/OpenIcon';
 
 import { Colors } from '@src/styles';
-import View from './View';
 
-type ModalProps = RNModalProps & {
+import View from '@src/components/View';
+
+type Props = ModalProps & {
   children: JSX.Element | JSX.Element[];
-  height?: number;
+  marginTop?: number;
   bgColor?: keyof typeof Colors;
   onKnobPress: () => void;
 };
 
-const ModalView = ({
+const Modal = ({
   children,
-  height = 100,
+  marginTop = 100,
   onKnobPress,
   bgColor = 'white',
   ...modalProps
-}: ModalProps) => (
-  <RNModal
-    {...modalProps}
-    swipeDirection="down"
-    backdropOpacity={0.8}
-    animationInTiming={500}
-    animationOutTiming={500}
-    hideModalContentWhileAnimating
-    animationIn="slideInUp"
-    animationOut="slideOutDown"
-    style={{ margin: 0, marginTop: height }}
-  >
-    <View variant="modal" alignCenter bgColor={bgColor}>
-      <View alignCenter spacing={{ my: 3 }} onPress={onKnobPress}>
-        <OpenIcon />
-      </View>
-      <>
-        {children}
-      </>
-    </View>
-  </RNModal>
-);
+}: Props) => {
+  const styles = useMemo(() => StyleSheet.create({
+    modal: { margin: 0, marginTop },
+  }), [marginTop]);
 
-export default ModalView;
+  return (
+    <RNModal
+      {...modalProps}
+      swipeDirection="down"
+      backdropOpacity={0.8}
+      animationInTiming={500}
+      animationOutTiming={500}
+      hideModalContentWhileAnimating
+      animationIn="slideInUp"
+      animationOut="slideOutDown"
+      style={styles.modal}
+    >
+      <View variant="modal" alignCenter bgColor={bgColor}>
+        <View alignCenter spacing={{ my: 3 }} onPress={onKnobPress}>
+          <OpenIcon />
+        </View>
+        <>
+          {children}
+        </>
+      </View>
+    </RNModal>
+  );
+};
+
+export default Modal;
