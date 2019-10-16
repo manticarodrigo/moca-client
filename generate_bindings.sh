085a4ee6c4027fe8a87bdb8b603e3aaa0750cc76
Rodrigo Mantica <rodrigo@approdite.com>
2019-10-16 00:20:53 -0400

    Updated openapi bindings.

--- generate_bindings.sh
+++ generate_bindings.sh
@@ -2,4 +2,4 @@
 
 rm -rfv src/services/openapi
 
-openapi-generator generate -g typescript-axios -i moca-api/moca.yaml -o src/services/openapi/
+openapi-generator generate -g typescript-axios -i moca-api/moca.yaml -o src/services/openapi/ --skip-validate-spec
