09eb80ac9e32b3bafd2958be90cf23bdc0c89eea
Ahmed <ahmed@approdite.com>
2019-10-18 17:30:45 +0200

    fixed dashboard screnn.

--- src/screens/DashboardScreen/DashboardScreen.tsx
+++ src/screens/DashboardScreen/DashboardScreen.tsx
@@ -9,8 +9,6 @@ import Image from '@src/components/Image';
 import Rating from '@src/components/Rating';
 import Button from '@src/components/Button';
 
-import AppointmentRequestCard from '@src/components/MessagingCards/AppointmentRequestCard';
-
 import {
   LowestPriceIcon,
   MorningIcon,
@@ -173,48 +171,47 @@ const DashboardScreen = () => {
               } = item;
 
               return (
-                // <View spacing={{ my: 2, p: 4 }} bgColor="white">
-                //   <View row justifyBetween>
-                //     <View row>
-                //       <Image rounded size={58} />
-                //       <View spacing={{ p: 2 }}>
-                //         <Text variant="titleSmall" spacing={{ mx: 3 }}>{name}</Text>
-                //         <Rating rate={rating} spacing={{ mx: 3 }} />
-                //       </View>
-                //     </View>
-                //     <View alignEnd spacing={{ py: 2 }}>
-                //       <View row>
-                //         <ClockIcon />
-                //         <Text variant="regular" spacing={{ ml: 2 }}>
-                //           {sessionDuration}
-                //           {' '}
-                //           mins
-                //         </Text>
-                //       </View>
-                //       <Text variant="titlePrimary" spacing={{ mt: 2 }}>
-                //         $
-                //         {''}
-                //         {sessionPrice}
-                //       </Text>
-                //     </View>
-                //   </View>
-                //   <View row justifyEnd spacing={{ mt: 1, mr: 5 }}>
-                //     <View variant="borderTopAndRight" spacing={{ py: 2, pl: 3, pr: 5 }}>
-                //       <Text variant="regularSmallGrey">
-                //         {experience}
-                //         {' '}
-                //         year of experience
-                //       </Text>
-                //     </View>
-                //     <View variant="borderTop" spacing={{ py: 2, px: 3 }}>
-                //       <Text variant="regularSmallGrey">
-                //         {licenseNumber}
-                //       </Text>
-                //     </View>
-                //   </View>
-                //   <Button variant="secondary" spacing={{ my: 2, mx: 6 }} onPress={handleButtonPress}>Message / Schedule</Button>
-                // </View>
-                <AppointmentRequestCard isPatient />
+                <View spacing={{ my: 2, p: 4 }} bgColor="white">
+                  <View row justifyBetween>
+                    <View row>
+                      <Image rounded size={58} />
+                      <View spacing={{ p: 2 }}>
+                        <Text variant="titleSmall" spacing={{ mx: 3 }}>{name}</Text>
+                        <Rating rate={rating} spacing={{ mx: 3 }} />
+                      </View>
+                    </View>
+                    <View alignEnd spacing={{ py: 2 }}>
+                      <View row>
+                        <ClockIcon />
+                        <Text variant="regular" spacing={{ ml: 2 }}>
+                          {sessionDuration}
+                          {' '}
+                          mins
+                        </Text>
+                      </View>
+                      <Text variant="titlePrimary" spacing={{ mt: 2 }}>
+                        $
+                        {''}
+                        {sessionPrice}
+                      </Text>
+                    </View>
+                  </View>
+                  <View row justifyEnd spacing={{ mt: 1, mr: 5 }}>
+                    <View variant="borderTopAndRight" spacing={{ py: 2, pl: 3, pr: 5 }}>
+                      <Text variant="regularSmallGrey">
+                        {experience}
+                        {' '}
+                        year of experience
+                      </Text>
+                    </View>
+                    <View variant="borderTop" spacing={{ py: 2, px: 3 }}>
+                      <Text variant="regularSmallGrey">
+                        {licenseNumber}
+                      </Text>
+                    </View>
+                  </View>
+                  <Button variant="secondary" spacing={{ my: 2, mx: 6 }} onPress={handleButtonPress}>Message / Schedule</Button>
+                </View>
               );
             }}
             keyExtractor={({ id }) => id}
