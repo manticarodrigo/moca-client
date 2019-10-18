3e0a51e0d50a90c42b2ab2887f6d6e6be6d890f8
Ahmed <ahmed@approdite.com>
2019-10-18 17:29:46 +0200

    added injuryCard.

--- src/components/MessagingCards/InjuryCard.tsx
+++ src/components/MessagingCards/InjuryCard.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import View from '@src/components/View';
+import Text from '@src/components/Text';
+import Image from '@src/components/Image';
+
 const InjuryCard = () => {
 
+  const InjuryExample = {
+    title: 'Neck Hernia',
+    details: 'Too bad my back is injured I need emergency therapy …',
+  }
 
-  return ();
+  const { title, details } = InjuryExample;
+
+  return (
+    <View row variant="curveBorder" bgColor="white" spacing={{ p: 3, pb: 5, m: 4 }}>
+      <View flex={3}>
+        <Text variant="titleSmallDark">My Injury</Text>
+        <Text variant="regularSemiGrey">{title}</Text>
+        <Text variant="smallDark" spacing={{ mt: 3 }}>{details}</Text>
+      </View>
+      <View flex={1} alignCenter spacing={{}}>
+        <Image rounded size={58} />
+      </View>
+    </View>
+  );
 };
 
 export default InjuryCard;
