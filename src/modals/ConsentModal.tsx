63ab6de78ec3cf3e65f32fae468d0c27ee209440
Rodrigo Mantica <manticarodrigo@gmail.com>
2019-11-25 06:59:49 -0500

    Integrated appointment request consent modal and confirmation alert.

--- src/modals/ConsentModal.tsx
+++ src/modals/ConsentModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Modal from 'react-native-modal';
 
 import View from '@src/components/View';
@@ -9,44 +9,40 @@ import Button from '@src/components/Button';
 
 type ConsentModalProps = {
   visible: boolean;
-  onPressButton?: () => void;
+  ptName: string;
+  onAccept?: () => void;
+  onClose: () => void;
 }
 
-const ConsentModal = ({ visible, onPressButton }: ConsentModalProps) => {
-  const [visibility, setVisibility] = useState(visible);
-
-  return (
-    <Modal
-      backdropOpacity={0.8}
-      isVisible={visibility}
-      animationInTiming={1000}
-      animationOutTiming={1000}
-      backdropTransitionInTiming={800}
-      backdropTransitionOutTiming={800}
-      onBackdropPress={() => setVisibility(!visibility)}
+const ConsentModal = ({ visible, ptName, onAccept, onClose }: ConsentModalProps) => (
+  <Modal
+    backdropOpacity={0.8}
+    isVisible={visible}
+    animationInTiming={500}
+    animationOutTiming={500}
+    backdropTransitionInTiming={300}
+    backdropTransitionOutTiming={300}
+    onBackdropPress={onClose}
+  >
+    <View
+      p={3}
+      variant="curveBorder"
+      bgColor="white"
     >
-      <View
-        flex={1}
-        my={8}
-        p={3}
-        variant="curveBorder"
-        bgColor="white"
-      >
-        <View alignEnd m={1} onPress={() => setVisibility(!visibility)}>
-          <CloseIcon />
-        </View>
-        <View alignCenter>
-          <ConsentIcon />
-        </View>
-        <View alignCenter my={5} mx={4}>
-          <Text m={1} variant="regularDark">By scheduling this appointment, you</Text>
-          <Text m={1} variant="regularDark">consent to treatment for all scheduled</Text>
-          <Text m={1} variant="regularDark">sessions with therapist for this injury.</Text>
-        </View>
-        <Button m={2} variant="primary" onPress={onPressButton}>Give Consent</Button>
+      <View alignEnd m={1} onPress={onClose}>
+        <CloseIcon />
       </View>
-    </Modal>
-  );
-};
+      <View alignCenter>
+        <ConsentIcon />
+      </View>
+      <View alignCenter my={5} mx={4}>
+        <Text m={1} variant="regularDark">
+          {`By scheduling this appointment, you consent to treatment for all scheduled sessions with ${ptName} for this injury.`}
+        </Text>
+      </View>
+      <Button m={2} variant="primary" onPress={onAccept}>Give Consent</Button>
+    </View>
+  </Modal>
+);
 
 export default ConsentModal;
